287ec71de4b1a591beecd86a9412ef69d32fc69b
farkuy <darkuy@mail.ru>
2024-11-13 23:34:23 +0300

    chunk translation

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import {render} from "react-dom";
-import "../src/shared/config/i18n/i18n"
+import "./shared/config/i18n/i18n"
 import App from "./app/App";
 import {ThemeProvider} from "app/providers/ThemeProviders";
 
