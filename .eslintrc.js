81f1daa018a4d8477a709d6a6a07dc933d65cf72
farkuy <darkuy@mail.ru>
2024-11-17 15:09:20 +0300

    fixed imports error

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,5 +41,6 @@ module.exports = {
         'no-unused-vars': 1,
         'i18next/no-literal-string': ['error', { markupOnly: true }],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'import/no-absolute-path': 0,
     },
 };
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 declare module '*.scss' {
     interface IClassNames {
         [className: string]: string
@@ -8,7 +6,12 @@ declare module '*.scss' {
     export = classNames;
 }
 
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.svg' {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default content;
+    import React from 'react';
+
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
 }
