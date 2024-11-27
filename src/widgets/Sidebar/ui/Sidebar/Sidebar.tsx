e853e6b0563bdd5fddf062cf910476a821ebf7df
farkuy <darkuy@mail.ru>
2024-11-27 21:45:34 +0300

    start fix

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import AboutIcon from 'shared/assets/icons/about.svg';
 import MainIcon from 'shared/assets/icons/main.svg';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -27,13 +28,21 @@ export const Sidebar = ({ className }: SidebarProps) => {
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
             <div className={classNames(cls.links)}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.link_wrapper}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                    className={cls.link_wrapper}
+                >
                     <MainIcon className={classNames(cls.svg)} />
                     <span className={classNames(cls.linkText)}>
                         {t('Главная')}
                     </span>
                 </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about" className={cls.link_wrapper}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                    className={cls.link_wrapper}
+                >
                     <AboutIcon className={classNames(cls.svg)} />
                     <span className={classNames(cls.linkText)}>
                         {t('О сайте')}
