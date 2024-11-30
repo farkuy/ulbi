80f6a5470a7e5b5ffc10cf152c4cea135923ec91
farkuy <darkuy@mail.ru>
2024-11-30 23:54:23 +0300

    base Modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,23 +1,41 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { FC } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
+    className?: string;
     isOpen: boolean;
     closeModal: () => void;
 }
 
 export const Modal:FC<ModalProps> = (props) => {
-    const { isOpen, closeModal, children } = props;
+    const {
+        isOpen, closeModal, className, children,
+    } = props;
 
     const mods: Record<string, boolean> = {
         [cls.open]: isOpen,
     };
 
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeModal();
+        }
+    }, [closeModal]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
     return (
         <div
             onClick={closeModal}
-            className={classNames(cls.Back, mods)}
+            className={classNames(cls.Back, mods, [className])}
         >
             <div
                 onClick={(e) => e.stopPropagation()}
