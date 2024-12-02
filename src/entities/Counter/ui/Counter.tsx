437a4873b2bfb23b92b38d9a4e37e318d2949ee5
farkuy <darkuy@mail.ru>
2024-12-02 23:28:20 +0300

    added redux

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from 'react';
 import { Button } from 'shared/ui/Button/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { StateSchema } from 'app/providers/StoreProvider/config/stateSchema';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/counterSlice';
 
 interface CounterProps {
     className?: string;
 }
 
 export const Counter:FC<CounterProps> = (props) => {
-    const count = useSelector((state: StateSchema) => state.counter.value);
+    const count = useSelector(getCounterValue);
     const dispatch = useDispatch();
-    const increment = () => { dispatch(increment()); };
-    const decrement = () => { dispatch(decrement()); };
+    const increment = () => { dispatch(counterActions.increment()); };
+    const decrement = () => { dispatch(counterActions.decrement()); };
     return (
         <div>
             <h1>
