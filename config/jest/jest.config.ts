cf338d06546721b88bf065908654ff64648c44c2
farkuy <darkuy@mail.ru>
2024-12-18 17:21:42 +0300

    added new global const

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -6,7 +6,7 @@
 import path from 'path';
 
 export default {
-    globals: { __IS_DEV__: true },
+    globals: { __IS_DEV__: true, __API__: 'http://localhost:8000' },
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { USER_AUTH_TOKEN } from 'shared/consts/auth';
 
 export const $axios = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: __API__,
     headers: {
         authorization: localStorage.getItem(USER_AUTH_TOKEN),
     },
