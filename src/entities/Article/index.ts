c72a60d98f8e266f1d8c195185b474f8a4f009da
farkuy <darkuy@mail.ru>
2025-01-22 17:23:43 +0300

    added types

---
+++ src/entities/Article/index.ts
@@ -0,0 +1,46 @@
+enum ArticleBlockType {
+    TEXT = 'TEXT',
+    CODE = 'CODE',
+    IMAGE = 'IMAGE'
+}
+
+interface ArticleBaseBlocks {
+    id: string;
+    type: ArticleBlockType;
+}
+
+interface ArticleTextBlock extends ArticleBaseBlocks {
+    type: ArticleBlockType.TEXT;
+    title?: string;
+    paragraphs: string[];
+}
+
+interface ArticleCodeBlock {
+    type: ArticleBlockType.CODE;
+    code: string;
+}
+
+interface ArticleImageBlock {
+    type: ArticleBlockType.IMAGE;
+    src: string;
+    title?: string;
+}
+
+export type ArticleBlocks = ArticleImageBlock | ArticleCodeBlock | ArticleTextBlock
+
+export enum ArticleType {
+    IT = 'IT',
+    SCIENCE = 'SCIENCE',
+    ECONOMICS = 'ECONOMICS',
+}
+
+export interface Article {
+    id: string;
+    title: string;
+    subtitle: string;
+    img: string;
+    views: number;
+    createdAt: string;
+    type: ArticleType[];
+    blocks: ArticleBlocks[];
+}
